8d4a96dc575e31e06faff61e105c59e7cf312da3
putra4648 <wakwaujaim@gmail.com>
2025-01-23 08:38:57 +0700

    🚧 Initial commit

---
+++ src/main/java/id/putra/peoplelibrarysystem/PeopleLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package id.putra.peoplelibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/putra/peoplelibrarysystem/PeopleLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package id.putra.peoplelibrarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

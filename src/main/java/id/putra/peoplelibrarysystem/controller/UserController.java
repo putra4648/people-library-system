59c80e6f692d2ab425dd941ffe43b3f91b22016f
Walkers <wakwaujaim@gmail.com>
2025-02-07 07:22:05 +0000

    🚩 Feat: added sample UI

--- src/main/java/id/putra/peoplelibrarysystem/controller/UserController.java
+++ src/main/java/id/putra/peoplelibrarysystem/controller/UserController.java
@@ -3,13 +3,16 @@ package id.putra.peoplelibrarysystem.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class PageController {
+@RequestMapping("user")
+public class UserController {
     @GetMapping("")
-    public ModelAndView index(ModelMap model) {
-        model.put("message", "People Library System");
-        return new ModelAndView("index", model);
+    public ModelAndView user(ModelMap model) {
+        model.put("message", "Hello User");
+        return new ModelAndView("user/index", model);
     }
+
 }

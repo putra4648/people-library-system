b300b3ccb9ae8a26b5a4d75161ffebbadc672c35
Walkers <wakwaujaim@gmail.com>
2025-02-11 13:23:16 +0700

    :wrench: Chore: update deploy configuration

--- src/main/java/id/putra/peoplelibrarysystem/controller/GlobalController.java
+++ src/main/java/id/putra/peoplelibrarysystem/controller/GlobalController.java
@@ -1,15 +1,6 @@
 package id.putra.peoplelibrarysystem.controller;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Request;
-import jakarta.ws.rs.core.Response;
-
-@Path("success")
 public class GlobalController {
-    @GET()
-    public void successHandler(Request request, Response response) {
 
-    }
 
 }
